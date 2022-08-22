0c09b29021864de1ef3e61a5ce7bf829af22f8f6
Jocelyn Naarden <tzionone@gmail.com>
2022-08-21 22:27:56 -0400

    Added code to sort front page

--- src/components/Patterns/PatternsList.js
+++ src/components/Patterns/PatternsList.js
@@ -2,64 +2,47 @@ import React from 'react'
 import { Header, Card } from 'semantic-ui-react'
 import Pattern from './Pattern'
 
-const PatternsList = ({ patternsObj, categoriesObject, fabricsObj, companiesObj, handleDelete }) => {
+const PatternsList = ({ companiesObj, handleDelete }) => {
 
-  console.log("From HOME", companiesObj[0])
   let patternsToDisplay;
-  console.log(companiesObj)
 
   if (companiesObj[0] === undefined) {
     return console.log("Undefined")
   } else {
-    console.log("From IF", companiesObj[0].name)
 
-    patternsToDisplay = companiesObj.map((company) => {
+    let patternsArr = []
 
-      return company.patterns.map((pattern) => {
-        console.log(pattern)
-        // console.debug("pattern id", pattern.id)
-        // console.debug("pattern id STUFF", pattern.company_id - 1)
-        // TODO: I forgot the easier way to do this D:
-        // TODO: Seems like name is undefined for something, can't figure it out
-        // console.error("TEST", pattern.company_id)
-        // console.error("TEST2", companiesObj[pattern.company_id - 1].name)
-        return <Pattern
-          id={pattern.id}
-          key={pattern.id}
-          companyName={companiesObj[pattern.company_id - 1].name}
-          extras={pattern.extras}
-          image={pattern.image}
-          notions={pattern.notions}
-          patternCode={pattern.pattern_code}
-          size={pattern.size}
-          yardage={pattern.yardage}
-          handleDelete={handleDelete}
-        //TODO: Is this because my pattern component is nested in an inner function??
-        />
+    function collectAllPatterns() {
+      companiesObj.map((company) => {
+        company.patterns.map((pattern) => {
+          patternsArr.push(pattern)
+        })
       })
+    }
+    collectAllPatterns()
 
+    patternsToDisplay = patternsArr.sort((prevPat, currPat) => {
+      const currTime = new Date(currPat.created_at)
+      const prevTime = new Date(prevPat.created_at)
+      return prevTime - currTime
+    }).map((pattern) => {
+      return <Pattern
+        id={pattern.id}
+        key={pattern.id}
+        companyName={companiesObj[pattern.company_id - 1].name}
+        extras={pattern.extras}
+        image={pattern.image}
+        notions={pattern.notions}
+        patternCode={pattern.pattern_code}
+        size={pattern.size}
+        createdAt={pattern.created_at}
+        yardage={pattern.yardage}
+        handleDelete={handleDelete}
+      />
     })
+
+
   }
-
-  // componentDidUpdate() {
-  //   console.log("FROM NEW FUNCTION", companiesObj[0]);
-  // }
-
-  // ********************************************************************
-  //TODO: THE ABOVE ISN'T WORKING WHEN I ADD .name... ALSO MULTIPLE CALLS SHOWN IN CONSOLE..
-  // WHY IS IT UNDEFINED??
-  // ********************************************************************
-
-
-
-
-  // console.log("From HOME2", companiesObj[0].id)
-
-  // companiesObj.map((pattern) => {
-  //   console.log(pattern)
-  // })
-
-
 
   return (
     <>
